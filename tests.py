1c860fd1ca555f3392d478eee59b970407bf7b80
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2017-08-14 23:29:17 -0400

    Add basic test skeleton and Travis CI setup
    Current tests are extremely minimal, but at least there is some
    structure to expand upon.
    
    This also enables Travis CI, and testing with multiple Python versions.

---
+++ tests.py
@@ -0,0 +1,29 @@
+"""Basic output validation tests"""
+
+import urbandict
+
+
+# pytest
+extra_plugin_dir = '.'
+pytest_plugins = ['errbot.backends.test']
+
+
+
+def test_with_no_arguments(testbot):
+    testbot.push_message('!ud')
+    result = testbot.pop_message()
+    assert result is not None
+
+
+def test_with_argument(testbot):
+    term = 'ahlie'
+    testbot.push_message('!ud ' + term)
+    result = testbot.pop_message()
+    assert term in result
+
+
+def test_without_argument(testbot):
+    term = 'some-totally-nonexistent-term'
+    testbot.push_message('!ud ' + term)
+    result = testbot.pop_message()
+    assert "No such term: " + term in result
